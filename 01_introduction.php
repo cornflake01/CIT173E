c38a1933438fe84a60a501b01c568b467c534f0a
cornflake01 <frncsclp@gmail.com>
2023-10-05 18:13:43 +0800

    intro

--- 01_introduction.php
+++ 01_introduction.php
@@ -2,11 +2,11 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport conten" ="width-device-width, initial-scale=1.0">  
+    <meta name="viewport content"="width-device-width, initial-scale=1.0">  
     <title>Hello World</title>
 </head>
 <body>
     <?php 
-    echo "Hello, testing!";?>
+    echo "Hello, testing testing!";?>
 </body>
 </html>
