18b1228221bf310a5287ba200cae8da462b86397
jekapacking <jekajournal@gmail.com>
2023-10-21 19:56:42 +0800

    review of php // refresh

---
+++ refresh.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Trying PHP again</title>
+</head>
+<body>
+
+<?php
+$number1 = 5;
+$number2 = 7;
+
+$sum = $number1 + $number2;
+
+echo "The sum of $number1 and $number2 is: $sum";
+?>
+
+</body>
+</html>
